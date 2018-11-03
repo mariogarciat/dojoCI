5bc67f4f9bb74fcfa5be65be3f1e7c1580f1ddb7
daniel.ospinap <daniel.ospinaperez@gmail.com>
2018-11-03 12:14:30 -0500

    tests test

---
+++ tests/codeBreaker_chai_expect.js
@@ -0,0 +1,17 @@
+var assert    = require("chai").assert;
+var CodeBreaker = require("../CodeBreaker");
+
+var codeBreaker = new CodeBreaker();
+
+describe("Tests de CodeBreaker ", function() {
+	describe("Check guessSecret Function: ", function() {
+		it("Check the returned value using: assert.equal(value,'value'): ", function() {
+			result   = codeBreaker.guess("1234");
+			assert.equal(result, "XXXX");
+		});
+	});		
+	
+	
+	
+	
+});
