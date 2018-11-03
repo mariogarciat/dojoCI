a1c531ff3b5e7b6521ec7a2b5aa3659e7ddf73a6
daniel.ospinap <daniel.ospinaperez@gmail.com>
2018-11-03 12:26:56 -0500

    test 1 funcionando

--- test/codeBreaker_chai_assert.js
+++ test/codeBreaker_chai_assert.js
@@ -6,8 +6,8 @@ var codeBreaker = new CodeBreaker();
 describe("Tests de CodeBreaker ", function() {
 	describe("Check guessSecret Function: ", function() {
 		it("Check the returned value using: assert.equal(value,'value'): ", function() {
-			result   = codeBreaker.guess("1234");
-			assert.equal(result, "XXXX");
+			result   = codeBreaker.guessSecret("1234");
+			assert.equal(result, "xxxx");
 		});
 	});		
 	
